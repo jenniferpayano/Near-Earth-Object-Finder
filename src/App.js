1407de38c32160ec3478df2d133cc64ff66b2aa0
jenniferpayano <arlenepayano@gmail.com>
2021-11-29 11:51:28 -0500

    Fixing Routes
    Routes Home-fix syntax

--- src/App.js
+++ src/App.js
@@ -1,38 +1,17 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route
+  Routes,
+ Route
 } from "react-router-dom";
 import Home from "./components/routes/Home";
 
-const routes = [
-  {
-    path: '/',
-    component: Home
-  }
-];
-function RouteWithSubRoutes(route) {
-  return (
-    <Route
-      path={route.path}
-      render={props => (
-        // pass the sub-routes down to keep nesting
-        <route.component {...props} routes={route.routes} />
-      )}
-    />
-  );
-}
 export default function App() {
   return (
-    <Router>
-      <div>
-        <Switch>
-          {routes.map((route, i) => (
-            <RouteWithSubRoutes key={i} {...route} />
-          ))}
-        </Switch>
-      </div>
-    </Router>
+    <Routes>
+
+      <Route exact path='/' element={<Home/>}/>
+    
+      </Routes>
+  
   );
 }
