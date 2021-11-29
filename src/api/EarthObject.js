7d97657e4f1d6f16a838a6e9b4e211cf2d51ccf6
jenniferpayano <arlenepayano@gmail.com>
2021-11-29 09:33:07 -0500

    Modified table to expect key.
    Created keys for table and components for the table

--- src/api/EarthObject.js
+++ src/api/EarthObject.js
@@ -2,7 +2,6 @@ import apiUrl from '../apiConfig'
 import axios from 'axios'
 
 export const earthObjectShow = (earthObjects) => {
-  console.log(earthObjects)
     return axios({
       url: apiUrl + "/feed?start_date=" + earthObjects.startDate +"&end_date=" + earthObjects.endDate + "&api_key=DEMO_KEY",
       method: 'GET'
